e7873c15a5613fe84ba37ca3bdea3b1e17ee8851
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2021-10-22 20:31:55 +0000

    Fixed it

--- lab02.py
+++ lab02.py
@@ -1,6 +1,6 @@
 print("Jeydin Pham\t\tSeptember 13, 2021")
 # Print alternating lines of plus signs and equal signs
-# Here's a row of pluses 
+# Here's a row of pluses
 print("+++++++++++++++++++++++++")
 # Now finish the program
 
